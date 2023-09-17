59bf86fb5cbb55424a18cdaf0ffac74ac87310ff
Caio-Santana <caio.santana@live.com>
2023-09-17 14:51:47 -0300

    Initial commit

---
+++ app/src/main/java/com/sam/workout/WorkoutDetailFragment.java
@@ -0,0 +1,21 @@
+package com.sam.workout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutDetailFragment extends Fragment {
+
+    public WorkoutDetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+}
