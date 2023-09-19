e12a3ce2c63c2d16ff16c6d319298b4014f2fdc7
Caio-Santana <caio.santana@live.com>
2023-09-19 20:57:26 -0300

    add workout list fragment

--- app/src/main/java/com/sam/workout/DetailActivity.java
+++ app/src/main/java/com/sam/workout/DetailActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
 
+    public static final String EXTRA_WORKOUT_ID = "id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,8 @@ public class DetailActivity extends AppCompatActivity {
         WorkoutDetailFragment frag = (WorkoutDetailFragment)
                 getSupportFragmentManager().findFragmentById(R.id.detail_frag);
 
-        frag.setWorkout(1);
+        int workoutId = getIntent().getIntExtra(EXTRA_WORKOUT_ID,0);
+
+        frag.setWorkout(workoutId);
     }
 }
